2412fadf1de3cc386b0b4f04f051b548347a8b54
user23007 <115121523+user23007@users.noreply.github.com>
2023-07-18 17:32:34 -0500

    Add files via upload

---
+++ 800/116A-CD.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int n,a,b;
+	cin >> n;
+	vector <vector<int>> v;
+	int k = 0;
+	vector<int> ans; 
+	for(int i = 0; i < n; i ++){
+		cin >> a;
+		cin >> b;
+		vector<int> num = {a,b};
+		v.push_back(num);
+	}
+	
+	for(int i = 0; i < n; i++){
+		k -= v[i][0];
+		k += v[i][1];
+		ans.push_back(k);
+	}
+	
+	auto resp = max_element(ans.begin(), ans.end());
+	cout << *resp;
+	return 0;
+}
