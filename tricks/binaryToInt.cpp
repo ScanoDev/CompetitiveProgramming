a21e2a8e21e1e57b635b04b0fb593a0671a7813b
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2024-11-11 07:03:46 -0500

    Add files via upload

---
+++ tricks/binaryToInt.cpp
@@ -0,0 +1,17 @@
+// Conversion into Binary code
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    auto number = 0b011;
+    cout << number;
+    return 0;
+}
+
+//source: Geeksforgeeks
+// time complexity: O(1)
+
+//La notación 0b se utiliza en C++ para 
+//representar un número en formato binario.
