eabb779766e99ca9c99015e7fb13e5ac99013609
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-06-29 18:44:15 -0500

    Add implementation for problem 1352C from codeforces

--- CodeForces/1200/1352C-1200CD.cpp
+++ CodeForces/1200/1352C-1200CD.cpp
@@ -1,8 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int H = INT_MAX;
-
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+        
+        long long full = (k - 1) / (n - 1);
+        long long pos = (k - 1) % (n - 1) + 1;
+        long long res = full * n + pos;
+        
+        cout << res << "\n";
+    }
+    
     return 0;
 }
