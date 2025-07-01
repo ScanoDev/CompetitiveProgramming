d97b26ef7b4bd399b1f7fe6e51875b9e4f7e6d71
Sebastián Cano Rincón <sebastiancano21@hotmail.es>
2025-07-01 09:09:46 -0500

    Add implementation of possibleStringCount function to count consecutive characters in a string. Problem from leetcode

---
+++ LeetCode/Strings/3330-LC.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int possibleStringCount(string word) {
+        int k = 1;
+        for(int i = 0; i < word.size()-1;i++){
+            if(word[i] == word[i+1]){
+                k++;
+            }
+        }
+        return k;
+    }
+};
